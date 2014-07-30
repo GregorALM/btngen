e3d273c1ea892a8c9854cbbf0df520ec0db3e37d
GregorALM <almgregor@gmail.com>
2014-07-30 14:44:21 +0600

    Написал свой виджет ColorPicker на JQurey-UI, добавил цвет рамки, изменил внешний вид слайдеров.

---
+++ js/colorpicker.js
@@ -0,0 +1,133 @@
+(function( $ ) {
+	$.widget( 'kataev.colorpicker',  {
+ 
+		
+		options: { 
+		 isCollapsed: true,
+		 valueR: 255,
+		 valueG: 255,
+		 valueB: 255,
+		 value: ''
+		},
+ 
+		
+		_create: function() {
+			$(this.element).addClass('ui-colorpicker').text('#');
+			$('<input/>', {'class': 'ui-colorpicker-hex'}).appendTo(this.element);
+			$('<div></div>', {'class': 'ui-colorpicker-rgb-sliders'}).appendTo(this.element);
+			$('<div></div>', {'class': 'ui-colorpicker-red-slider', 'color': 'red'}).appendTo(this.element.children('.ui-colorpicker-rgb-sliders'));
+			$('<div></div>', {'class': 'ui-colorpicker-green-slider', 'color': 'green'}).appendTo(this.element.children('.ui-colorpicker-rgb-sliders'));
+			$('<div></div>', {'class': 'ui-colorpicker-blue-slider', 'color': 'blue'}).appendTo(this.element.children('.ui-colorpicker-rgb-sliders'));
+			$('.ui-colorpicker-red-slider').slider({
+				animate: true,
+				min: 0,
+				max: 255,
+				range: 'min',
+				value: this.options.valueR
+			});
+			$('.ui-colorpicker-green-slider').slider({
+				animate: true,
+				min: 0,
+				max: 255,
+				range: 'min',
+				value: this.options.valueG
+			});
+			$('.ui-colorpicker-blue-slider').slider({
+				animate: true,
+				min: 0,
+				max: 255,
+				range: 'min',
+				value: this.options.valueB
+			});
+
+			this.setUpListeners();
+		},
+ 
+		_init: function () {
+			this.setRGB(this.options.value);
+			this.element.find('.ui-colorpicker-hex').val(this.options.value);
+			this.setBG(this.options.value);
+		},
+ 
+ 		//Деструктор не стал писать, нет времени(надеюсь никто не собирается в этом приложении деинициализировать виджет)
+		destroy: function() {
+			
+		},
+
+		getHEX: function () {
+			var r = parseInt(this.options.valueR).toString(16),
+				g = parseInt(this.options.valueG).toString(16),
+				b = parseInt(this.options.valueB).toString(16);
+				if (r.length === 1) {if (this.options.valueR > 15) {r += '0';} else {r = '0' + r;}}
+				if (g.length === 1) {if (this.options.valueG > 15) {g += '0';} else {g = '0' + g;}}
+				if (b.length === 1) {if (this.options.valueB > 15) {b += '0';} else {b = '0' + b;}}
+			return r + g + b;
+		},
+
+		setRGB: function (hex) {
+			if (hex.length === 6) {
+				this.options.valueR = parseInt(hex.substring(0,2),16);
+				this.options.valueG = parseInt(hex.substring(2,4),16);
+				this.options.valueB = parseInt(hex.substring(4,6),16);
+			}	
+		},
+
+		setBG: function (hex, e) {
+			this.element.children('.ui-colorpicker-hex').css('background-color', '#' + hex);
+			this.options.value = hex;
+			var ui = {};
+			ui.handle = this.element.children('.ui-colorpicker-hex').get();
+			ui.value = this.options.value;
+			this._trigger('change', e, ui);
+		},
+
+		value: function () {
+			return this.options.hex;
+		},
+
+		changeColor: function (e, ui) {
+			switch ($(e.target).attr('color')) {
+				case 'red':
+				e.data.options.valueR = ui.value;
+				break;
+				case 'green':
+				e.data.options.valueG = ui.value;
+				break;
+				case 'blue':
+				e.data.options.valueB = ui.value;
+				break;
+			}
+			e.data.element.find('.ui-colorpicker-hex').val(e.data.getHEX());
+			e.data.setBG(e.data.getHEX());
+		},
+
+		changeByKey: function (e) {
+			e.data.setRGB($(e.target).val());
+			console.log(e.data.getHEX());
+			e.data.setBG(e.data.getHEX());
+			e.data.setSliders();
+		},
+
+		setSliders: function () {
+			this.element.find('.ui-colorpicker-red-slider').slider('value', this.options.valueR);
+			this.element.find('.ui-colorpicker-green-slider').slider('value', this.options.valueG);
+			this.element.find('.ui-colorpicker-blue-slider').slider('value', this.options.valueB);
+		},
+
+		setUpListeners: function () {
+			this.element.on({
+				'mouseenter': function(){
+					$(this).children('.ui-colorpicker-rgb-sliders').slideDown(200);
+				},
+				'mouseleave': function(){
+					$(this).children('.ui-colorpicker-rgb-sliders').slideUp(200);
+					
+				}
+				
+			});
+			this.element.children('.ui-colorpicker-hex').on('keyup', this, this.changeByKey);
+			this.element.find('.ui-colorpicker-red-slider, .ui-colorpicker-green-slider, .ui-colorpicker-blue-slider').bind('slide', this, this.changeColor);
+		}
+
+	});
+}( jQuery ) );
--- js/main.js
+++ js/main.js
@@ -1,55 +1,105 @@
+//@codekit-prepend "colorpicker.js"
+
 (function () {
 
 	var btnGen = {
 
 		settings: {
 			'border-radius': {
-				type: "slider",
+				type: 'slider',
 				initial: {
 					animate: true,
 					min: 0,
 					max: 50,
-					range: "min",
-					value: 0,
+					range: 'min',
+					value: 2
 				}
 			},
 			'border-size': {
-				type: "slider",
+				type: 'slider',
 				initial: {
 					animate: true,
 					min: 0,
 					max: 15,
-					range: "min",
-					value: 0
+					range: 'min',
+					value: 3
+				}
+			},
+			'border-color': {
+				type: 'colorpicker',
+				initial: {
+					value: 'ff0000'
 				}
 			}
 		},
 
+		currentValue: {},
+
 		initialize: function () {
 			this.setUpListeners();
 			this.setUpOptoinsUI();
-			this.setValue();
+		},
+
+		setUpListeners: function () {
+			$(this.getIDs('slider')).bind('slide', this.changeCurrentValue);
+			$(this.getIDs('colorpicker')).bind('colorpickerchange', this.changeCurrentValue);
+		},
+
+		getIDs: function (type) {
+			var output = '';
+			$.each(btnGen.settings, function (key, val) {
+				if (val.type === type) {
+					output += '#' + key + ', ';
+				}
+			});
+			return output.slice(0, output.length - 2);
 		},
 
 		setUpOptoinsUI: function () {
 			$.each(this.settings, function (key, val) {
 				switch (val.type) {
-					case "slider":
+					case 'slider':
 						$("#" + key).slider(val.initial);
+						break;
+					case 'colorpicker':
+						$("#" + key).colorpicker(val.initial);
+						break;
 				}
 			});
+			$.each(this.settings, function (key, val) {
+				btnGen.currentValue[key] = val.initial.value;
+			});
+			this.outputValue();
 		},
 
-		setUpListeners: function () {
-			$("#border-radius, #border-size").bind("create slide change", this.changeValue);
+		changeCurrentValue: function (event, ui) {
+			var option = $(ui.handle).parent().attr('id');
+			console.log(option);
+			btnGen.currentValue[option] = ui.value;
+			btnGen.outputValue();
 		},
 
-		// setValue: function (settings) {
-
-		// },
-
-		changeValue: function (event, ui) {
-			console.log(ui.value);
+		outputValue: function () {
+			var html = '<button id="generated-button">',
+				css = '';
+			$.each(this.currentValue, function (key, val) {
+				switch (key) {
+					case 'border-size':
+						if (val > 0) {css += 'border: ' + val + 'px solid #' + btnGen.currentValue['border-color'] + '; \n';}
+						break;
+					case 'border-radius':
+						if (val > 0) {css += 'border-radius: ' + val + '%; \n';}
+						break;
+				}
+			});
+			$('#result-button').attr('style', css.slice(0, css.length - 3));
+			html += '</button>';
+			$('#result-html').text(html);
+			if (css !== '') {
+				$('#result-css').text('#generated-button {\n' + css + '}');
+			} else {
+				$('#result-css').text('');
+			}
 		}
 
 	};
